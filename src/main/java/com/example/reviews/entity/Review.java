4e7be9dcbd43ab170410e39542576e0a2d4fd060
IegorSoboliev <evsobolev@gmail.com>
2020-03-02 14:52:52 +0200

    HW38_1 : reestablished project from scratch with right naming. Added User & Review entities

---
+++ src/main/java/com/example/reviews/entity/Review.java
@@ -0,0 +1,18 @@
+package com.example.reviews.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "reviews")
+public class Review {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Long productId;
+    @Column (columnDefinition = "TEXT")
+    private String text;
+}
